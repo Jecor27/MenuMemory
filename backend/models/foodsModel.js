b4401bcda233abe0b8324593bd4524a8432ead02
Jesus <jjcorrales76@gmail.com>
2025-03-12 09:51:59 -0600

    added a new model and a new route file

---
+++ backend/models/foodsModel.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+const FoodSchema = new Schema({
+    name: { type: String, required: true, unique: true },
+    ingredients: [
+        {
+            name: { type: String, required: true },
+            amount: { type: String, required: true },
+            unit: { type: String }
+        }
+    ],
+    instructions: { type: String, required: true }
+});
+
+export default mongoose.model('Food', FoodSchema);
