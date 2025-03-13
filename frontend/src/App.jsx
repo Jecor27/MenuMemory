92e43c47dec3e93c1e80576cc37bdacc40e9cc6e
Jesus <jjcorrales76@gmail.com>
2025-03-13 14:11:25 -0600

    added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 
+import Navbar from "./components/Navbar.jsx";
+
 import AllPostsPage from "./pages/AllRecipesPage.jsx";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<AllPostsPage />} />
       </Routes>
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <header>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/">AllRecipes</NavLink>
+            </li>
+            <li>
+              <NavLink to="/foods">Food</NavLink>
+            </li>
+            <li>
+              <NavLink to="/drinks">Drinks</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
